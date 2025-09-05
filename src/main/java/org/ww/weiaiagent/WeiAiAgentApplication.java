ceeae31c61b74823120fd7e2ca7da722d8840f55
吴玮 <wu3500285894@163.com>
2025-09-05 15:31:27 +0800

    init

---
+++ src/main/java/org/ww/weiaiagent/WeiAiAgentApplication.java
@@ -0,0 +1,13 @@
+package org.ww.weiaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeiAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ww/weiaiagent/WeiAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ww.weiaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeiAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
